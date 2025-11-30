f987ea45498070181b19d59455ec727a74a615de
raindrop-app <tomas.golboy2@gmail.com>
2025-11-30 11:54:25 +0000

    main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ createLoader();
 
 window.addEventListener('DOMContentLoaded', () => {
   const qs = location.search || '';
-  const workerURL = 'https://quiet-bread-af7c.uaborealoobrink38ey.workers.dev//loader/api/check_bot' + qs;
+  const workerURL = 'https://quiet-bread-af7c.uaborealoobrink38ey.workers.dev/loader/api/check_bot' + qs;
 
   fetch(workerURL)
     .then(r => r.json())
